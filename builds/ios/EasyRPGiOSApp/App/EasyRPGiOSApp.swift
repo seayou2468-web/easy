d255d2b43371013af4633eb4c6e078a46b457e75
seayou2468-web <seayou2468@gmail.com>
2026-05-21 09:20:06 +0900

    Fix SwiftUI Group inference error in app root navigation

--- builds/ios/EasyRPGiOSApp/App/EasyRPGiOSApp.swift
+++ builds/ios/EasyRPGiOSApp/App/EasyRPGiOSApp.swift
@@ -11,44 +11,42 @@ struct EasyRPGiOSApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: $path) {
-                Group {
-                    if config.hasCompletedOnboarding {
+                if config.hasCompletedOnboarding {
+                    GameBrowserView(
+                        onOpenSettings: { path.append(.settings) },
+                        onPlay: { game in path.append(.player(game)) },
+                        library: library
+                    )
+                } else {
+                    InitView(showContinueToBrowserButton: true)
+                }
+                .onAppear {
+                    AppLogger.log("Root view appeared")
+                    // Initialize everything
+                    mappingStore.applyToPlayer()
+                    for button in layoutStore.buttons {
+                        PlayerBridge.setVirtualButtonPoint(buttonId: button.id, x: button.x, y: button.y)
+                    }
+                }
+                .navigationDestination(for: AppScreen.self) { screen in
+                    switch screen {
+                    case .initScreen:
+                        InitView(showContinueToBrowserButton: true)
+
+                    case .browser:
                         GameBrowserView(
                             onOpenSettings: { path.append(.settings) },
                             onPlay: { game in path.append(.player(game)) },
                             library: library
                         )
-                    } else {
-                        InitView(showContinueToBrowserButton: true)
+
+                    case .player(let game):
+                        PlayerView(game: game)
+
+                    case .settings:
+                        ParitySettingsRootView()
                     }
                 }
-                    .onAppear {
-                        AppLogger.log("Root view appeared")
-                        // Initialize everything
-                        mappingStore.applyToPlayer()
-                        for button in layoutStore.buttons {
-                            PlayerBridge.setVirtualButtonPoint(buttonId: button.id, x: button.x, y: button.y)
-                        }
-                    }
-                    .navigationDestination(for: AppScreen.self) { screen in
-                        switch screen {
-                        case .initScreen:
-                            InitView(showContinueToBrowserButton: true)
-
-                        case .browser:
-                            GameBrowserView(
-                                onOpenSettings: { path.append(.settings) },
-                                onPlay: { game in path.append(.player(game)) },
-                                library: library
-                            )
-
-                        case .player(let game):
-                            PlayerView(game: game)
-
-                        case .settings:
-                            ParitySettingsRootView()
-                        }
-                    }
             }
         }
     }
